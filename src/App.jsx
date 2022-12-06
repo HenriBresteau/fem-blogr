8332bb25c22f5187de21f0655ec6a1e455539354
Henri Bresteau <henri.bresteau@gamil.com>
2022-12-06 21:25:00 +0100

    Import fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import Footer from "./Components/Footer/Footer";
 import Nav from "./Components/Navbar/Nav";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <div>
         <Nav />
       </div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-overpass underline">Hello world!</h1>
       <Footer />
     </div>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF525D",
+        primaryLigth: "#FF7A85",
+        secondary: "#1F3F5B",
+        footer: "#C8C8CB",
+        body: "#4B5862",
+        footerBg: "#25252D",
+      },
+      fontFamily: {
+        ubuntu: ["Ubuntu", "cursive"],
+        overpass: ["Overpass", "cursive"],
+      },
+      backgroundImage: {
+        "hero-pattern":
+          "linear-gradient(to bottom, hsl(13deg 100% 72% / 90%), hsl(353deg 100% 62% /90%)), url(/images/bg-pattern-intro-mobile.svg)",
+      },
+    },
   },
   plugins: [],
 };
